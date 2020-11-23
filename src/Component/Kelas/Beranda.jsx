c2bede12bff53d04ba5cd606977c8780e2acd07a
Ramdoni007 <ramd42156@gmail.com>
2020-11-23 10:35:59 +0700

    Penambahan Materi Kelas Component dan Component Fungsional

---
+++ src/Component/Kelas/Beranda.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Beranda extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hayy Downy !!! Ini Adalah Beranda dari Kelas Component!!!</h1>
+      </div>
+    );
+  }
+}
+
+export default Beranda;
